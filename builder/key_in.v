347482b1601682ef2a079a9275dc8f78d3c6f1bb
knknkn1162 <knknkn1162@gmail.com>
2019-01-08 21:17:37 +0900

    modify key_in module

--- builder/key_in.v
+++ builder/key_in.v
@@ -7,12 +7,13 @@ module key_in (
   input wire clk, i_sclr, i_en,
   // key0, key1, key2, key3, key4
   input wire i_key_right_n, i_key_down_n, i_key_left_n, i_key_char_n, i_key_backspace_n,
-  output wire [7:0] o_ascii,
-  output wire o_ascii_en,
-  output wire o_right_en, o_down_en, o_left_en
+  output wire o_type,
+  output wire [7:0] o_asciiex,
+  output wire o_asciiex_en
 );
 
-  wire s_key_char_en, s_key_backspace_en;
+  wire s_char_en, s_backspace_en;
+  wire s_right_en, s_down_en, s_left_en;
 
   parameter BITS = 3;
   parameter TRIGGER = 3'b011;
@@ -20,32 +21,54 @@ module key_in (
   // arrow key
   posedge_detector #(BITS, TRIGGER) posedge_right(
     .clk(clk), .i_sclr(i_sclr), .i_en(i_en), .i_dat(~i_key_right_n),
-    .o_posedge(o_right_en)
+    .o_posedge(s_right_en)
   );
 
   posedge_detector #(BITS, TRIGGER) posedge_down(
     .clk(clk), .i_sclr(i_sclr), .i_en(i_en), .i_dat(~i_key_down_n),
-    .o_posedge(o_down_en)
+    .o_posedge(s_down_en)
   );
 
   posedge_detector #(BITS, TRIGGER) posedge_left(
     .clk(clk), .i_sclr(i_sclr), .i_en(i_en), .i_dat(~i_key_left_n),
-    .o_posedge(o_left_en)
+    .o_posedge(s_left_en)
   );
 
   posedge_detector #(BITS, TRIGGER) posedge_char(
     .clk(clk), .i_sclr(i_sclr), .i_en(i_en), .i_dat(~i_key_char_n),
-    .o_posedge(s_key_char_en)
+    .o_posedge(s_char_en)
   );
 
   posedge_detector #(BITS, TRIGGER) posedge_backspace(
     .clk(clk), .i_sclr(i_sclr), .i_en(i_en), .i_dat(~i_key_backspace_n),
-    .o_posedge(s_key_backspace_en)
+    .o_posedge(s_backspace_en)
   );
 
   // BS or 'a'
-  assign o_ascii = (s_key_backspace_en) ? 8'h08 : 8'h41;
-  assign o_ascii_en = s_key_char_en | s_key_backspace_en;
+  assign o_asciiex = asciiex(s_char_en, s_backspace_en, s_right_en, s_down_en, s_left_en);
+  assign o_type = s_char_en | s_backspace_en;
+  assign o_asciiex_en = s_char_en | s_backspace_en | s_right_en | s_down_en | s_left_en;
+
+  function [7:0] asciiex;
+    input char_en;
+    input backspace_en;
+    input right_en;
+    input down_en;
+    input left_en;
+    begin
+      if (backspace_en) begin
+        asciiex = 8'h08;
+      end else if (s_char_en) begin
+        asciiex = 8'h41;
+      end else if (s_right_en) begin
+        asciiex = 8'h02;
+      end else if (s_down_en) begin
+        asciiex = 8'h03;
+      end else if (s_left_en) begin
+        asciiex = 8'h04;
+      end
+    end
+  endfunction
 
 endmodule
 
--- builder/top.v
+++ builder/top.v
@@ -15,42 +15,44 @@ module top (
   output wire [3:0] o_ledr
 );
 
-  wire [7:0] s_ascii0, s_ascii1;
-  wire [4:0] s_ledr_en;
+  //wire [7:0] s_ascii0, s_ascii1;
+  //wire [4:0] s_ledr_en;
+  wire s_type;
+  wire [7:0] s_asciiex;
+  wire s_asciiex_en;
 
   key_in key_in0 (
     .clk(clk), .i_sclr(i_sclr), .i_en(1'b1),
     .i_key_right_n(i_key_right_n), .i_key_down_n(i_key_down_n), .i_key_left_n(i_key_left_n), .i_key_char_n(i_key_char_n), .i_key_backspace_n(i_key_backspace_n),
-    .o_ascii(s_ascii0), .o_ascii_en(s_ledr_en[0]),
-    .o_right_en(s_ledr_en[1]),
-    .o_down_en(s_ledr_en[2]),
-    .o_left_en(s_ledr_en[3])
+    .o_type(s_type),
+    .o_asciiex(s_asciiex),
+    .o_asciiex_en(s_asciiex_en)
   );
 
   // for debug
-  genvar i;
-  generate
-    for(i = 0; i < 4; i=i+1) begin: gen_ledr
-      toggle #(1'b0) toggle0 (
-        .clk(clk), .i_sclr(i_sclr), .i_en(s_ledr_en[i]),
-        .o_sw(o_ledr[i])
-      );
-    end
-  endgenerate
+  //genvar i;
+  //generate
+  //  for(i = 0; i < 4; i=i+1) begin: gen_ledr
+  //    toggle #(1'b0) toggle0 (
+  //      .clk(clk), .i_sclr(i_sclr), .i_en(s_ledr_en[i]),
+  //      .o_sw(o_ledr[i])
+  //    );
+  //  end
+  //endgenerate
 
-  flopr_en #(8) flopr_ascii (
-    .clk(clk), .i_sclr(i_sclr), .i_en(s_ledr_en[0]),
-    .i_a(s_ascii0),
-    .o_y(s_ascii1)
-  );
-  hex_decoder hex_decoder0 (
-    .i_num(s_ascii1[3:0]),
-    .o_seg7(o_hex0)
-  );
-  hex_decoder hex_decoder1 (
-    .i_num(s_ascii1[7:4]),
-    .o_seg7(o_hex1)
-  );
+  //flopr_en #(8) flopr_ascii (
+  //  .clk(clk), .i_sclr(i_sclr), .i_en(s_ledr_en[0]),
+  //  .i_a(s_ascii0),
+  //  .o_y(s_ascii1)
+  //);
+  //hex_decoder hex_decoder0 (
+  //  .i_num(s_ascii1[3:0]),
+  //  .o_seg7(o_hex0)
+  //);
+  //hex_decoder hex_decoder1 (
+  //  .i_num(s_ascii1[7:4]),
+  //  .o_seg7(o_hex1)
+  //);
 
 endmodule
 
